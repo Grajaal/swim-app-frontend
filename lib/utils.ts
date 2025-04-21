c840d6d2e3c5a50dab7cf8195bc51d67bc192639
Grajaal <jorgegrajal@gmail.com>
2025-04-21 16:55:09 +0200

    feat(utils): add util function to determine unique color for each swimmer

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function stringToHslColor(str: string, s = 50, l = 80): string {
+  let hash = 0
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash)
+  }
+  const h = hash % 360
+  return `hsl(${h}, ${s}%, ${l}%)`
 }
