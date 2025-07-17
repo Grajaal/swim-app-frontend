73dad8e7e14b90786f940b11860ecdd49593bc42
Grajaal <jorgegrajal@gmail.com>
2025-07-17 21:22:56 +0200

    refactor: remove Google login button and related elements from login and register forms
    - Deleted the Google login button and its associated visual elements from both the login and register forms.
    - Adjusted tabIndex values for accessibility improvements in the registration link.

--- components/forms/login-form.tsx
+++ components/forms/login-form.tsx
@@ -87,21 +87,10 @@ export function LoginForm({
         <Button disabled={isSubmitting} type="submit" className='w-full cursor-pointer' tabIndex={3}>
           Iniciar sesión
         </Button>
-        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-          <span className="relative z-10 bg-background px-2 text-muted-foreground">
-            O continúa con
-          </span>
-        </div>
-        <Button variant="outline" className="w-full" tabIndex={5}>
-          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 30 30">
-            <path d="M 15.003906 3 C 8.3749062 3 3 8.373 3 15 C 3 21.627 8.3749062 27 15.003906 27 C 25.013906 27 27.269078 17.707 26.330078 13 L 25 13 L 22.732422 13 L 15 13 L 15 17 L 22.738281 17 C 21.848702 20.448251 18.725955 23 15 23 C 10.582 23 7 19.418 7 15 C 7 10.582 10.582 7 15 7 C 17.009 7 18.839141 7.74575 20.244141 8.96875 L 23.085938 6.1289062 C 20.951937 4.1849063 18.116906 3 15.003906 3 z"></path>
-          </svg>
-          Iniciar sesión con Google
-        </Button>
       </div>
       <div className="text-center text-sm">
         ¿No tienes cuenta?{" "}
-        <a href="/register" className="underline underline-offset-4" tabIndex={6}>
+        <a href="/register" className="underline underline-offset-4" tabIndex={5}>
           Regístrate
         </a>
       </div>
